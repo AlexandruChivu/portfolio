434550a96b0aec15870fb71ee4e55a34c3cb6c53
AlexandruChivu <alexandru.chivu.it@gmail.com>
2023-10-27 22:59:35 +0300

    update files

---
+++ server.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, url_for, redirect, jsonify
+import os, csv
+
+# Determine the directory of server.py
+script_dir = os.path.dirname(__file__)
+
+# Create a relative path to database.txt
+database_path = os.path.join(script_dir, 'database.csv')
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.get_json()  # Get JSON data from the request
+            name = data["name"]
+            email = data["email"]
+            message = data["message"]
+
+            # Write the data to the file
+            with open(database_path, mode='a', newline='') as database:
+                csv_writer = csv.writer(database)
+
+                # Check if the file is empty, and if so, write the header
+                if os.path.getsize(database_path) == 0:
+                    csv_writer.writerow(["name", "email", "message"])
+                
+                csv_writer.writerow([name,email,message])
+
+            return jsonify({"message": "Data successfully submitted"})
+        except Exception as e:
+            return jsonify({"error": str(e)}), 400
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
